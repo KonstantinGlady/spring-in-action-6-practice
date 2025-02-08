61691cf8caa498d10b33be127bd7e61629f91780
Glady <gladyi@hotmail.com>
2025-02-08 21:02:34 +0300

    HomeController

---
+++ chapter01/src/test/java/tacos/TacoCloudApplicationTest.java
@@ -0,0 +1,14 @@
+package tacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TacoCloudApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
