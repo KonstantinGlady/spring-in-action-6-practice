501446c38588fe6b4d7a23559505bfa427b30f38
Glady <gladyi@hotmail.com>
2025-02-12 13:17:54 +0300

    validation

--- chapter01/src/main/java/tacos/TacoOrder.java
+++ chapter01/src/main/java/tacos/TacoOrder.java
@@ -1,20 +1,39 @@
 package tacos;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class TacoOrder {
 
+    @NotBlank(message = "Name required")
     private String deliveryName;
+
+    @NotBlank(message = "Street required")
     private String deliveryStreet;
+
+    @NotBlank(message = "City required")
     private String deliveryCity;
+
+    @NotBlank(message = "State required")
     private String deliveryState;
+
+    @NotBlank(message = "zip required")
     private String deliveryZip;
+
+    @CreditCardNumber(message = "Not a valid credit card")
     private String ccNumber;
+
+    @Pattern(regexp = "^(0[1-9]|1[0-2])(\\/)([2-9][0-9])$")
     private String ccExpiration;
+
+    @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
 
     private List<Taco> tacos = new ArrayList<>();
--- chapter01/src/main/java/tacos/web/OrderController.java
+++ chapter01/src/main/java/tacos/web/OrderController.java
@@ -3,9 +3,13 @@ package tacos.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import tacos.TacoOrder;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -20,7 +24,14 @@ public class OrderController {
     }
 
     @PostMapping()
-    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+    public String processOrder(@Valid TacoOrder order,
+                               Errors errors,
+                               SessionStatus sessionStatus) {
+
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+
         log.info("Order submitted {}", order);
         sessionStatus.setComplete();
 
